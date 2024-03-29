84e9fad328081f2a6a7bb0e4b1b9318037ab9e00
Nathan Galindo <nathan.zebedee@gmail.com>
2024-03-29 15:43:26 -0500

    added email folder

--- emails/CateringRequest.tsx
+++ emails/CateringRequest.tsx
@@ -32,17 +32,15 @@ const baseUrl = process.env.VERCEL_URL
   : "";
 
 export const CateringRequest = ({
-  username,
-  userImage,
-  invitedByUsername,
-  invitedByEmail,
-  teamName,
-  teamImage,
-  inviteLink,
-  inviteFromIp,
-  inviteFromLocation,
+  firstName,
+  lastName,
+  email,
+  phone,
+  partySize,
+  date,
+  message
 }: CateringRequestProps) => {
-  const previewText = `Join ${invitedByUsername} on Vercel`;
+  const previewText = `New Catering Resevation by ${firstName}`;
 
   return (
     <Html>
@@ -61,20 +59,20 @@ export const CateringRequest = ({
               />
             </Section>
             <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-              Join <strong>{teamName}</strong> on <strong>Vercel</strong>
+              {/* Join <strong>{teamName}</strong> on <strong>Vercel</strong> */}
             </Heading>
             <Text className="text-black text-[14px] leading-[24px]">
-              Hello {username},
+              {/* Hello {username}, */}
             </Text>
             <Text className="text-black text-[14px] leading-[24px]">
-              <strong>{invitedByUsername}</strong> (
+              {/* <strong>{invitedByUsername}</strong> ( */}
               <Link
-                href={`mailto:${invitedByEmail}`}
+                // href={`mailto:${invitedByEmail}`}
                 className="text-blue-600 no-underline"
               >
-                {invitedByEmail}
+                {/* {invitedByEmail} */}
               </Link>
-              ) has invited you to the <strong>{teamName}</strong> team on{" "}
+              {/* ) has invited you to the <strong>{teamName}</strong> team on{" "} */}
               <strong>Vercel</strong>.
             </Text>
             <Section>
@@ -82,7 +80,7 @@ export const CateringRequest = ({
                 <Column align="right">
                   <Img
                     className="rounded-full"
-                    src={userImage}
+                    // src={userImage}
                     width="64"
                     height="64"
                   />
@@ -98,7 +96,7 @@ export const CateringRequest = ({
                 <Column align="left">
                   <Img
                     className="rounded-full"
-                    src={teamImage}
+                    // src={teamImage}
                     width="64"
                     height="64"
                   />
@@ -108,24 +106,24 @@ export const CateringRequest = ({
             <Section className="text-center mt-[32px] mb-[32px]">
               <Button
                 className="bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3"
-                href={inviteLink}
+                // href={inviteLink}
               >
                 Join the team
               </Button>
             </Section>
             <Text className="text-black text-[14px] leading-[24px]">
               or copy and paste this URL into your browser:{" "}
-              <Link href={inviteLink} className="text-blue-600 no-underline">
-                {inviteLink}
-              </Link>
+              {/* <Link href={inviteLink} className="text-blue-600 no-underline"> */}
+                {/* {inviteLink} */}
+              {/* </Link> */}
             </Text>
             <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
             <Text className="text-[#666666] text-[12px] leading-[24px]">
               This invitation was intended for{" "}
-              <span className="text-black">{username}</span>. This invite was
-              sent from <span className="text-black">{inviteFromIp}</span>{" "}
+              {/* <span className="text-black">{username}</span>. This invite was */}
+              {/* sent from <span className="text-black">{inviteFromIp}</span>{" "} */}
               located in{" "}
-              <span className="text-black">{inviteFromLocation}</span>. If you
+              {/* <span className="text-black">{inviteFromLocation}</span>. If you */}
               were not expecting this invitation, you can ignore this email. If
               you are concerned about your account's safety, please reply to
               this email to get in touch with us.
@@ -137,16 +135,16 @@ export const CateringRequest = ({
   );
 };
 
-CateringRequest.PreviewProps = {
-  username: "alanturing",
-  userImage: `${baseUrl}/static/vercel-user.png`,
-  invitedByUsername: "Alan",
-  invitedByEmail: "alan.turing@example.com",
-  teamName: "Enigma",
-  teamImage: `${baseUrl}/static/vercel-team.png`,
-  inviteLink: "https://vercel.com/teams/invite/foo",
-  inviteFromIp: "204.13.186.218",
-  inviteFromLocation: "São Paulo, Brazil",
-} as CateringRequestProps;
+// CateringRequest.PreviewProps = {
+//   username: "alanturing",
+//   userImage: `${baseUrl}/static/vercel-user.png`,
+//   invitedByUsername: "Alan",
+//   invitedByEmail: "alan.turing@example.com",
+//   teamName: "Enigma",
+//   teamImage: `${baseUrl}/static/vercel-team.png`,
+//   inviteLink: "https://vercel.com/teams/invite/foo",
+//   inviteFromIp: "204.13.186.218",
+//   inviteFromLocation: "São Paulo, Brazil",
+// } as CateringRequestProps;
 
 export default CateringRequest;
