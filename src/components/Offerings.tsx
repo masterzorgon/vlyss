6bb99e901f458ed6bac12dd89f078ca79ea0b7ca
Nathan Galindo <nathan.zebedee@gmail.com>
2024-06-20 09:59:42 -0500

    switch icn color

--- src/components/Offerings.tsx
+++ src/components/Offerings.tsx
@@ -49,11 +49,11 @@ function DeviceArrowIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M9 0a4 4 0 00-4 4v24a4 4 0 004 4h14a4 4 0 004-4V4a4 4 0 00-4-4H9zm0 2a2 2 0 00-2 2v24a2 2 0 002 2h14a2 2 0 002-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9z"
-        fill="#737373"
+        fill="#f36cff"
       />
       <path
         d="M12 25l8-8m0 0h-6m6 0v6"
-        stroke="#171717"
+        stroke="#f36cff"
         strokeWidth={2}
         strokeLinecap="round"
       />
@@ -71,7 +71,7 @@ function DeviceCardsIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M9 0a4 4 0 00-4 4v24a4 4 0 004 4h14a4 4 0 004-4V4a4 4 0 00-4-4H9zm0 2a2 2 0 00-2 2v24a2 2 0 002 2h14a2 2 0 002-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9z"
-        fill="#737373"
+        fill="#f36cff"
       />
       <path
         fillRule="evenodd"
@@ -79,7 +79,7 @@ function DeviceCardsIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
         d="M9 13a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H10a1 1 0 01-1-1v-2zm0 6a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H10a1 1 0 01-1-1v-2zm1 5a1 1 0 00-1 1v2a1 1 0 001 1h12a1 1 0 001-1v-2a1 1 0 00-1-1H10z"
         fill={`url(#${id}-gradient)`}
       />
-      <rect x={9} y={6} width={14} height={4} rx={1} fill="#171717" />
+      <rect x={9} y={6} width={14} height={4} rx={1} fill="#f36cff" />
       <circle cx={16} cy={16} r={16} fill="#A3A3A3" fillOpacity={0.2} />
       <defs>
         <linearGradient
@@ -90,8 +90,8 @@ function DeviceCardsIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
           y2={28}
           gradientUnits="userSpaceOnUse"
         >
-          <stop stopColor="#737373" />
-          <stop offset={1} stopColor="#737373" stopOpacity={0} />
+          <stop stopColor="#f36cff" />
+          <stop offset={1} stopColor="#f36cff" stopOpacity={0} />
         </linearGradient>
       </defs>
     </svg>
@@ -106,13 +106,13 @@ function DeviceClockIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M5 4a4 4 0 014-4h14a4 4 0 014 4v10h-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9a2 2 0 00-2 2v24a2 2 0 002 2h5v2H9a4 4 0 01-4-4V4z"
-        fill="#737373"
+        fill="#f36cff"
       />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M24 32a8 8 0 100-16 8 8 0 000 16zm1-8.414V19h-2v5.414l4 4L28.414 27 25 23.586z"
-        fill="#171717"
+        fill="#f36cff"
       />
     </svg>
   )
@@ -125,14 +125,14 @@ function DeviceListIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M9 0a4 4 0 00-4 4v24a4 4 0 004 4h14a4 4 0 004-4V4a4 4 0 00-4-4H9zm0 2a2 2 0 00-2 2v24a2 2 0 002 2h14a2 2 0 002-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9z"
-        fill="#737373"
+        fill="#f36cff"
       />
-      <circle cx={11} cy={14} r={2} fill="#171717" />
-      <circle cx={11} cy={20} r={2} fill="#171717" />
-      <circle cx={11} cy={26} r={2} fill="#171717" />
+      <circle cx={11} cy={14} r={2} fill="#f36cff" />
+      <circle cx={11} cy={20} r={2} fill="#f36cff" />
+      <circle cx={11} cy={26} r={2} fill="#f36cff" />
       <path
         d="M16 14h6M16 20h6M16 26h6"
-        stroke="#737373"
+        stroke="#f36cff"
         strokeWidth={2}
         strokeLinecap="square"
       />
@@ -149,13 +149,13 @@ function DeviceLockIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M5 4a4 4 0 014-4h14a4 4 0 014 4v10h-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9a2 2 0 00-2 2v24a2 2 0 002 2h5v2H9a4 4 0 01-4-4V4z"
-        fill="#737373"
+        fill="#f36cff"
       />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M18 19.5a3.5 3.5 0 117 0V22a2 2 0 012 2v6a2 2 0 01-2 2h-7a2 2 0 01-2-2v-6a2 2 0 012-2v-2.5zm2 2.5h3v-2.5a1.5 1.5 0 00-3 0V22z"
-        fill="#171717"
+        fill="#f36cff"
       />
     </svg>
   )
@@ -168,17 +168,17 @@ function DeviceChartIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M9 0a4 4 0 00-4 4v24a4 4 0 004 4h14a4 4 0 004-4V4a4 4 0 00-4-4H9zm0 2a2 2 0 00-2 2v24a2 2 0 002 2h14a2 2 0 002-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9z"
-        fill="#737373"
+        fill="#f36cff"
       />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M23 13.838V26a2 2 0 01-2 2H11a2 2 0 01-2-2V15.65l2.57 3.212a1 1 0 001.38.175L15.4 17.2a1 1 0 011.494.353l1.841 3.681c.399.797 1.562.714 1.843-.13L23 13.837z"
-        fill="#171717"
+        fill="#f36cff"
       />
       <path
         d="M10 12h12"
-        stroke="#737373"
+        stroke="#f36cff"
         strokeWidth={2}
         strokeLinecap="square"
       />
