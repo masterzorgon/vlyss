99da0cfa2f8d93a0bb907fead0a4ee2655bedf3a
Nathan Galindo <nathan.zebedee@gmail.com>
2024-06-26 00:14:13 -0500

    change route

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -11,6 +11,6 @@ export const resend = new Resend(process.env.RESEND_KEY! as string);
 export const audienceId = process.env.RESEND_AUDIENCE_ID! as string;
 export const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET! as string;
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY! as string, {
-    apiVersion: "2023-08-16",
+    apiVersion: "2024-04-10",
     typescript: true,
 });
