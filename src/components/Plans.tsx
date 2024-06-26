ff5ada1fe9e96d5d6d5b9ab745c1e5bf1a776f5b
Nathan Galindo <nathan.zebedee@gmail.com>
2024-06-26 09:20:59 -0500

    switched out custom links

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -230,7 +230,7 @@ export function Plans() {
                         Simple Pricing. Simple Onboarding.
                     </h2>
                     <p className="mt-2 text-lg text-gray-600">
-                        Join the Vlyss family by clicking &quot;Get started today&quot;<br />Pause or cancel anytime
+                        Join the Vlyss family by clicking &quot;Get started today.&quot; Pause or cancel anytime.
                     </p>
                 </div>
 
